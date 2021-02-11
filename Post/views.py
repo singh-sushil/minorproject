bafd592101dc49bcdeb967c159dd20ac6136363e
AashishGh <aashishghimire9999@gmail.com>
2021-02-11 22:38:13 +0545

    Modifying Postdetails ambiguity

--- Post/views.py
+++ Post/views.py
@@ -43,9 +43,13 @@ class Post_List_authenticated(ListView):
     model=Post
     template_name='post_list_authenticated.html'
 
-class PostDetailView(ListView):
+class PostDetailAuthenticatedView(ListView):
     model = Post
-    template_name = 'postdetail.html'    
+    template_name = 'postdetailauthenticated.html'   
+
+class PostDetailOutsideView(ListView):
+    model = Post
+    template_name = 'postdetailoutside.html'  
 
 class PostSuccess(TemplateView):
     template_name = 'post_success.html'
