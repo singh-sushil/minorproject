b6f6e0440b1f82560c015d61f4d8d15dd568efc4
suraj <surajido111@gmail.com>
2021-03-03 13:04:01 +0545

    final edit by suraj

--- realestate/views.py
+++ realestate/views.py
@@ -7,6 +7,17 @@ class Homepage(TemplateView):
     template_name = 'homepage.html'
 '''
 from django.shortcuts import render
+from Post.models import Post
+from Post.forms import PostForm
+from django.utils import timezone
+# from django.db import connection as cn
+
+data = Post.objects.filter(
+    published_date__lte=timezone.now()).order_by('-published_date')
 def HomePage(request):
+    # cursor = cn.cursor()
+    # q1 = "select * from post_post"
+    # cursor.execute(q1)
+    # data = cursor.fetchall()
     print(request.user.id)
-    return render(request,'homepage.html',{'uid':request.user.id})
+    return render(request,'homepage.html',{'uid':request.user.id,'data':data})
